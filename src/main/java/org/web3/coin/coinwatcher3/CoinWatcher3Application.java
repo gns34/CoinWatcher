4a247e8516cc0a98410126642c5ff986d9290b52
GNS VS <gnsgns6@outlook.com>
2024-06-20 20:59:29 +0530

    Getting Started

---
+++ src/main/java/org/web3/coin/coinwatcher3/CoinWatcher3Application.java
@@ -0,0 +1,21 @@
+package org.web3.coin.coinwatcher3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
+public class CoinWatcher3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinWatcher3Application.class, args);
+    }
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/org/web3/coin/coinwatcher3/CoinWatcher3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.web3.coin.coinwatcher3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinWatcher3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
