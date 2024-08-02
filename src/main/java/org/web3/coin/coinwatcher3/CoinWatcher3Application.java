c56d5b774274f005c1738f88afa3111d693fdf94
gns <gnsgns199@gmail.com>
2024-08-02 19:14:09 +0530

    Start

--- src/main/java/org/web3/coin/coinwatcher3/CoinWatcher3Application.java
+++ src/main/java/org/web3/coin/coinwatcher3/CoinWatcher3Application.java
@@ -16,6 +16,6 @@ public class CoinWatcher3Application {
     }
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+        return String.format("Hello Mr. - %s!", name);
     }
 }
