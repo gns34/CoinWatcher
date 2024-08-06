18fc177afcbf9b2486d3f27c4e35c4e34ff05724
gns <gnsgns199@gmail.com>
2024-08-06 21:04:53 +0530

    updating UserRepository.java

--- src/main/java/org/web3/coin/coinwatcher3/repository/UserRepository.java
+++ src/main/java/org/web3/coin/coinwatcher3/repository/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.stereotype.Repository;
 import org.web3.coin.coinwatcher3.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User findByEmail(String email);
 
 }
