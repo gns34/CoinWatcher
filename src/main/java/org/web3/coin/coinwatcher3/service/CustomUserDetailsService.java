d8caf410e9b9c803e063d5940089ea7cecabaa67
gns <gnsgns199@gmail.com>
2024-08-06 21:07:25 +0530

    loadbyusername

---
+++ src/main/java/org/web3/coin/coinwatcher3/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package org.web3.coin.coinwatcher3.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.web3.coin.coinwatcher3.entities.User;
+import org.web3.coin.coinwatcher3.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        List<GrantedAuthority> authorityList=new ArrayList<>();
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorityList);
+    }
+}
